2a8f30d3383936549126732432f06fba8b0d8590
Emmett Schneider <emmett.f.schneider@gmail.com>
2019-01-17 13:09:45 -0600

    first commit

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+export default class LocationList  extends Component {
+    render() {
+        return (
+            <article>
+                <h1>Location List</h1>
+                <section>Nashville North: 6000 Nolensville Rd, Nashville, TN</section>
+                <section>Nashville South: 100 Ewing Dr, Nashville, TN</section>
+            </article>
+        );
+    }
+}
