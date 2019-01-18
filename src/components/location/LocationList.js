09a05ae9a0122aeb5a1cd6083761a9160411fd08
Emmett Schneider <emmett.f.schneider@gmail.com>
2019-01-18 12:06:06 -0600

    completed locations portion of practice, need to add owners

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 
 
-export default class LocationList  extends Component {
+class LocationList extends Component {
     render() {
+      console.log(this.props.locations)
         return (
-            <article>
-                <h3>Student Kennels</h3>
-                <h4>Nashville North Location</h4>
-                <h5>500 Puppy Way</h5>
-            </article>
-        );
+            <section className="locations">
+            {
+                this.props.locations.map(location =>
+                    <div key={location.id}>
+                        <h4>{location.name}</h4>
+                        <p>{location.address}</p>
+                    </div>
+                )
+            }
+            </section>
+        )
     }
 }
+
+export default LocationList
